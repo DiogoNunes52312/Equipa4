d3e9d83e6d76fb0bfcd6d912b69687ef128f7f08
DiogoNunes52312 <diogonunessilva192006@gmail.com>
2025-10-26 16:44:27 +0000

    base de daos mysql atualizada
    adicionamos a tabela notificacao

--- gestao_eventos_upt.sql
+++ gestao_eventos_upt.sql
@@ -115,6 +115,35 @@ LOCK TABLES `inscricao` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `notificacao`
+--
+
+DROP TABLE IF EXISTS `notificacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notificacao` (
+  `notificacao_id` int NOT NULL AUTO_INCREMENT,
+  `utilizador_id` int NOT NULL,
+  `titulo` varchar(150) NOT NULL,
+  `mensagem` text NOT NULL,
+  `lida` tinyint(1) DEFAULT '0',
+  `data_envio` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`notificacao_id`),
+  KEY `fk_notificacao_utilizador` (`utilizador_id`),
+  CONSTRAINT `fk_notificacao_utilizador` FOREIGN KEY (`utilizador_id`) REFERENCES `utilizador` (`id_utilizador`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notificacao`
+--
+
+LOCK TABLES `notificacao` WRITE;
+/*!40000 ALTER TABLE `notificacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notificacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `utilizador`
 --
 
@@ -153,4 +182,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-25 16:19:32
+-- Dump completed on 2025-10-26 16:43:00
